7dbf812afe65beeec383190f33c1bd96046c2e5c
Artemka007 <kopylovartyom007@gmail.com>
2021-03-09 23:00:39 +0500

    Second commit

--- account/urls.py
+++ account/urls.py
@@ -6,4 +6,5 @@ from account.views import RegisterView
 urlpatterns = [
     path('login/', views.LoginView.as_view(), name='login'),
     path('register/', RegisterView.as_view(), name='register'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
 ]
--- mainApp/views.py
+++ mainApp/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'content/index.html')
+def about(request):
+    return render(request, 'content/about_project.html')
+def contacts(request):
+    return render(request, 'content/contacts.html')
