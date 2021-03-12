44fc56e3824328bd7d1d83bce8839521866be5d5
artem <kopylovartyom007@gmail.com>
2021-03-12 22:43:15 +0500

    Design and functions for upload files

--- formDecryption/static/js/upload.js
+++ formDecryption/static/js/upload.js
@@ -10,7 +10,7 @@ $(function() {
     // Как закинули файл
     $('#upload').fileupload({
         // Куда его можно закинуть
-        dropZone: $('#drop'),
+        dropZone: ul,
 
         // Формат отправки
         dataType: 'json',
@@ -24,20 +24,20 @@ $(function() {
         done: function(e, data) {
             var proc_container = $('' +
                 '<div class="upload-image-container" data-pk="' + data.result.pk + '">' +
-                '   <div class="progress_c_bar"></div>' +
-                '   <div style="display: flex; flex-direction: column; justify-content: center;object-fit: cover; width: 45px; height: 45px; margin: -5px -10px;">' +
-                '      <img src="' + data.result.url_redirect + '" alt=""/>' +
+                '   <div class="container dir_row" style="width: 100%; margin: 5px 10px;">' +
+                '      <div class="progress_c_bar"></div>' +
+                '      <div style="display: flex; flex-direction: column; justify-content: center;object-fit: cover; width: 45px; height: 45px; margin: -10px 10px;">' +
+                '         <img src="' + data.result.url_redirect + '" alt=""/>' +
+                '      </div>'+
+                '      <div style="padding: 5px 10px; border-right: rgba(0,0,0,0.4) solid 1px; border-left: rgba(0,0,0,0.4) solid 1px;">' + data.result.form_name + '</div>' +
+                '      <div style="padding: 5px 10px;  border-right: rgba(0,0,0,0.4) solid 1px;">' + formatFileSize(data.result.form_size) + '</div>'+
                 '   </div>'+
-                '   <div style="margin-left: 20px;">' + formatFileSize(data.result.form_size) + '</div>'+
-                '   <div style="margin-left: 5px;">' + data.result.form_name + '</div>'+
                 '   <div data-url="/account/profile/photos/delete/" data-action="del_photo" data-id="' + data.result.pk + '" class="question_about_edit_message_close" style="display: flex; justify-content: flex-end; float:right; width: 100%; ">\n' +
                 '        <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">\n' +
                 '            <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>\n' +
                 '        </svg>\n' +
                 '    </div>' +
                 '</div>').appendTo(ul);
-            proc_container.find('p').text(data.files[0].name)
-                .append('<i>' + formatFileSize(data.files[0].size) + '</i>');
             $('.spinner').css({'display': 'none', 'z-index': '0'})
             $('.blackout_container').css({'z-index': '-9', 'background-color': 'rgba(0,0,0,0)'})
 
