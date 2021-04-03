e745fdd1ee0a3fa0f5665c898ca68f71ca7fbee7
Artemka007 <kopylovartyom007@gmail.com>
2021-04-03 12:30:39 +0500

    Pre-final commit

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+from mainApp.secrets import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'hk+6wd@u!8s!506m(s)uknsy0*$kyhdpyrtvz!s#+tk0mee5q#'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
